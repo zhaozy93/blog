49d9b6af7ffa98ae591b279130bf7113f392ef5e
zhaozhengyi <zhaozy93@outlook.com>
2018-03-18 09:13:25 +0800

    [UPDATE]梳理

---
+++ py/py3/1.py
@@ -0,0 +1,30 @@
+a = 'a'
+if a:
+    print(a)
+else:
+    print('else')
+
+print('--------')
+
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
+
+print('--------')
+
+print(11 / 3)
+print(9 / 3)
+print(9 % 3)
+print(11 // 3)
+
+print('--------')
+
+print(ord('0'))
+print(ord('a'))
+print(chr(66))
+print(chr(48))
+
+print('--------')
+
+
